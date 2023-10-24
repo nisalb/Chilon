1a57d9110c5c139ad6ac2d6f3c643bbb0c858834
Nisal Bandara <nisalb@hey.com>
2023-10-24 22:37:24 +0530

    add basic text editing requirements

---
+++ init.el
@@ -0,0 +1,87 @@
+;; General emacs configurations
+(setq straight-repository-branch "develop")
+
+;; UI preferences
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+
+;; Separate dirs for backups and auto saves
+(setq require-final-newline t
+      create-lockfiles nil
+      make-backup-files nil
+      ; backup-by-copying t ; uncomment this when enabling backup files
+      auto-save-default t
+      auto-save-list-file-prefix (concat user-emacs-directory ".cache/saves/")
+      auto-save-file-name-transforms
+      (list (list "\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'"
+                  ;; Prefix tramp autosaves to prevent conflicts with local ones
+                  (concat auto-save-list-file-prefix "tramp-\\2") t)
+            (list ".*" auto-save-list-file-prefix t)))
+
+;; Setup straight and use-package for package management
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; And then configure straight to use use-package by default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
+
+;; Get alabaster theme
+;; Alabaster is a minimal, no-clutter, soothing theme. It makes programming feel cleaner and fun.
+(straight-use-package
+ '(alabaster :type git
+	     :host github
+	     :repo "nisalb/alabaster-emacs"))
+(load-theme 'alabaster t)
+
+;; Next I need the support for ligatures.
+(use-package ligature
+  :config
+  ;; Enable ligatures in all programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; But I don't need them in C. Because, I want to feel C as a low level language
+  (ligature-set-ligatures 'c-mode '("www" "ff" "fi" "ffi"))
+  (global-ligature-mode t))
+
+;; Now the basic UI requirements are complete.
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka Custom" :foundry "nil" :slant normal :weight regular :height 130 :width normal)))))
