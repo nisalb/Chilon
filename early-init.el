39f4b641a866c344c97dbdb673632c94ceea6def
Nisal Bandara <nisalb@hey.com>
2023-10-25 07:56:15 +0530

    redirect eln-cache to .cache dir

--- early-init.el
+++ early-init.el
@@ -1,2 +1,7 @@
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
+
+;; Redirect eln-cache
+(setq native-comp-eln-load-path (cdr native-comp-eln-load-path))
+(push (expand-file-name ".cache/eln-cache/" user-emacs-directory)
+      native-comp-eln-load-path)
