8fd96a91f1a8a99252d05b0278a6f612c0f74e96
Nisal Bandara <nisalb@hey.com>
2023-12-01 17:51:31 +0530

    Add paredit mode

--- init.el
+++ init.el
@@ -168,6 +168,10 @@
    (emacs-lisp . t)
    (lisp . t)))
 
+;; Paredit mode for Lisps
+(use-package paredit
+  :hook ((emacs-lisp-mode lisp-mode clojure-mode lisp-interaction-mode scheme-mode) . paredit-mode))
+
 ;; VCS Integration
 (require 'vcs)
 
@@ -193,4 +197,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Lamda Mono" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "Lambda Mono" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
